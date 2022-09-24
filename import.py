66624f6292d6dbc4ad3ac39969db1ef790ad6e46
Felix Wotschofsky <hello@felisk.io>
2022-09-24 18:04:04 +0200

    Add items import script 📀

---
+++ import.py
@@ -0,0 +1,18 @@
+
+from pymongo import MongoClient
+import csv
+
+mongodb_client = MongoClient('mongodb://localhost:27017')
+database = mongodb_client['ceipt']
+
+
+with open('items.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        doc = database['items'].insert_one({
+            'name': row['description'],
+            'isVegan': row['isVegan'] == '1',
+            'isVegetarian': row['isVegetarian'] == '1',
+            'weightGrams': float(row['Weight per part'].replace('kg', '').replace('l', '').replace(',', '.')) * 1000,
+            'co2ProKg': float(row['kgCo2Produced'].replace(',', '.'))
+        })
