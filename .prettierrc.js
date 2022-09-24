596f48a068849413daa8727c5dce032fc8a234fa
Felix Wotschofsky <hello@felisk.io>
2022-09-24 13:05:13 +0200

    Add prettier config ✨

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^@contenized/(.*)$',
+    '^@/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderCaseInsensitive: true,
+};
