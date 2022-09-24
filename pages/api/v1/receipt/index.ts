904a5a68f1aba257b48c383207e0e5f389ac315b
Felix Wotschofsky <hello@felisk.io>
2022-09-24 18:06:38 +0200

    Format code ✨

--- pages/api/v1/receipt/index.ts
+++ pages/api/v1/receipt/index.ts
@@ -3,7 +3,9 @@ import nc from 'next-connect';
 
 import receiptController from '../../../../controllers/receipt.controller';
 
-export default nc({}).get(async (_req: NextApiRequest, res: NextApiResponse) => {
-  const data = await receiptController.getAll();
-  res.json({ ok: true, data });
-});
+export default nc({}).get(
+  async (_req: NextApiRequest, res: NextApiResponse) => {
+    const data = await receiptController.getAll();
+    res.json({ ok: true, data });
+  }
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+
 import Button from '../components/Button';
 import Header from '../components/Header';
-
 import ImageInput, { FullFileValue } from '../components/ImageInput';
 import ReceiptCard from '../components/ReceiptCard';
 import apiClient from '../services/apiClient';
@@ -29,92 +29,74 @@ const defaultState: DefaultState = { type: 'DEFAULT' };
 export default function Page() {
   const [pageState, setPageState] = useState<PageState>(defaultState);
 
-
-
   const [receipts, setReceipts] = useState<any[]>([]);
 
   const useFormValue = useForm({ shouldUseNativeValidation: true });
 
   const { handleSubmit, watch } = useFormValue;
 
-
-
-
-
   const onSubmit = async (data: { fileValue: FullFileValue }) => {
-
     const { fileValue } = data;
 
     setPageState({ type: 'LOADING', fileData: fileValue });
 
-    const receiptData = await apiClient().getReceiptFromImg(fileValue.file)
+    const receiptData = await apiClient().getReceiptFromImg(fileValue.file);
 
     setPageState({ type: 'READY_TO_SUBMIT', fileData: fileValue, receiptData });
   };
 
   useEffect(() => {
+    if (!watch('fileValue')?.hasFile) return;
 
-    if (!watch("fileValue")?.hasFile) return
-
-
-    handleSubmit(onSubmit)()
-
-
-
-  }, [watch("fileValue")])
+    handleSubmit(onSubmit)();
+  }, [watch('fileValue')]);
 
   if (pageState.type === 'DEFAULT')
     return (
       <div
         style={{
-          position: "relative",
-          top: "20rem",
+          position: 'relative',
+          top: '20rem',
           height: 'calc(100vh - 25rem)',
           display: 'flex',
           alignItems: 'center',
           flexDirection: 'column',
         }}
       >
-
-        <div style={{ paddingBottom: "2rem" }}>
+        <div style={{ paddingBottom: '2rem' }}>
           <ImageInput useFormValue={useFormValue} field="fileValue" />
-
         </div>
 
         {/* <Button onClick={handleSubmit(onSubmit)} label="get receipt from image" /> */}
       </div>
-
-
     );
   if (pageState.type === 'READY_TO_SUBMIT') {
-
     const postDings = async () => {
+      const data = await apiClient().postReceipt(pageState.receiptData);
 
-      const data = await apiClient().postReceipt(pageState.receiptData)
+      alert(JSON.stringify(data, null, 2));
+    };
 
-      alert(JSON.stringify(data, null, 2))
-    }
+    return (
+      <div
+        style={{
+          position: 'relative',
+          top: '20rem',
+          height: 'calc(100vh - 25rem)',
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <div style={{ width: 'fit-content' }}>
+          <ReceiptCard receipt={pageState.receiptData} />
 
-    return <div
-      style={{
-        position: "relative",
-        top: "20rem",
-        height: 'calc(100vh - 25rem)',
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }}
-    >
+          <br style={{ height: '2rem' }} />
 
-      <div style={{ width: "fit-content" }}>
-        <ReceiptCard receipt={pageState.receiptData} />
-
-        <br style={{ height: "2rem" }} />
-
-        <Button onClick={postDings} label="Post Receipt" />
+          <Button onClick={postDings} label="Post Receipt" />
+        </div>
       </div>
-    </div>;
-
+    );
   }
 
   // useEffect(() => {
