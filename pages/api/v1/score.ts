5a81174c91a9dc56b0a2e732a12622cbca716504
Felix Wotschofsky <hello@felisk.io>
2022-09-25 08:17:43 +0200

    Extract score calculation logic into separate file 🚛

--- pages/api/v1/score.ts
+++ pages/api/v1/score.ts
@@ -1,62 +1,20 @@
-import {
-  TranslateClient,
-  TranslateTextCommand,
-} from '@aws-sdk/client-translate';
-import Fuse from 'fuse.js';
 import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
-import fs from 'node:fs/promises';
-import path from 'node:path';
-import Papa from 'papaparse';
 
+import calculateScore from '../../../utils/calculateScore';
 import initMongoose from '../../../utils/initMongoose';
-
-const client = new TranslateClient({ region: 'eu-central-1' });
 
 const handler = nc();
 
 handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
   await initMongoose();
 
-  const dbData = await fs.readFile(
-    path.resolve('./data/SEL CF for users-Tabelle 1.csv'),
-    'utf8'
-  );
-  const parsedDb = Papa.parse(dbData, {
-    delimiter: ';',
-    header: true,
-    // @ts-ignore
-  }).data.map((item: Record<string, string>) => ({
-    group: item['FOOD COMMODITY GROUP'],
-    item: item['Food commodity ITEM'],
-    footprint:
-      item['Carbon Footprint kg CO2eq/kg or l of food ITEM'] &&
-      parseFloat(
-        item['Carbon Footprint kg CO2eq/kg or l of food ITEM'].replace(',', '.')
-      ),
-    typology: item['Food commodity TYPOLOGY'],
-  }));
-  const index = Fuse.createIndex(['item'], parsedDb);
-  const fuse = new Fuse(parsedDb, {}, index);
-
   const productScores = await Promise.all(
     req.body.products.map(async (p: any) => {
-      const data = await client.send(
-        new TranslateTextCommand({
-          Text: p.label,
-          SourceLanguageCode: 'de',
-          TargetLanguageCode: 'en',
-        })
-      );
-
-      const results = fuse.search(data.TranslatedText as string, {
-        limit: 1,
-      });
-
       return {
         quantity: p.quantity,
         label: p.label,
-        score: results.length > 0 ? results[0].item.footprint : null,
+        score: await calculateScore(p.label),
       };
     })
   );
---
+++ utils/calculateScore.ts
@@ -0,0 +1,68 @@
+import {
+  TranslateClient,
+  TranslateTextCommand,
+} from '@aws-sdk/client-translate';
+import Fuse from 'fuse.js';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import Papa from 'papaparse';
+
+let fuseInstance: Fuse<{
+  group: string;
+  item: string;
+  footprint: number | '';
+  typology: string;
+}>;
+
+const loadDatabase = async () => {
+  if (fuseInstance) {
+    return fuseInstance;
+  }
+
+  const dbData = await fs.readFile(
+    path.resolve('./data/SEL CF for users-Tabelle 1.csv'),
+    'utf8'
+  );
+  const parsedDb = Papa.parse(dbData, {
+    delimiter: ';',
+    header: true,
+    // @ts-ignore
+  }).data.map((item: Record<string, string>) => ({
+    group: item['FOOD COMMODITY GROUP'],
+    item: item['Food commodity ITEM'],
+    footprint:
+      item['Carbon Footprint kg CO2eq/kg or l of food ITEM'] &&
+      parseFloat(
+        item['Carbon Footprint kg CO2eq/kg or l of food ITEM'].replace(',', '.')
+      ),
+    typology: item['Food commodity TYPOLOGY'],
+  }));
+  const index = Fuse.createIndex(['item'], parsedDb);
+  const fuse = new Fuse(parsedDb, {}, index);
+
+  fuseInstance = fuse;
+
+  return fuse;
+};
+
+const client = new TranslateClient({ region: 'eu-central-1' });
+
+const calculateScore = async (productLabel: string) => {
+  const fuse = await loadDatabase();
+
+  const data = await client.send(
+    new TranslateTextCommand({
+      Text: productLabel,
+      SourceLanguageCode: 'de',
+      TargetLanguageCode: 'en',
+    })
+  );
+
+  const results = fuse.search(data.TranslatedText as string, {
+    limit: 1,
+  });
+
+  return results.length > 0 ? results[0].item.footprint : null;
+};
+
+export default calculateScore;
