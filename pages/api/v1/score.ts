55197c540892365f40a982a81fcd23e940834815
Felix Wotschofsky <hello@felisk.io>
2022-09-25 02:04:18 +0200

    Use real data for scoring 🧑‍🔬

--- pages/api/v1/score.ts
+++ pages/api/v1/score.ts
@@ -1,5 +1,9 @@
+import Fuse from 'fuse.js';
 import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import Papa from 'papaparse';
 
 import initMongoose from '../../../utils/initMongoose';
 
@@ -8,11 +12,34 @@ const handler = nc();
 handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
   await initMongoose();
 
+  const dbData = await fs.readFile(
+    path.resolve('./data/SEL CF for users-Tabelle 1.csv'),
+    'utf8'
+  );
+  const parsedDb = Papa.parse(dbData, {
+    delimiter: ';',
+    header: true,
+  }).data.map((item) => ({
+    group: item['FOOD COMMODITY GROUP'],
+    item: item['Food commodity ITEM'],
+    footprint:
+      item['Carbon Footprint kg CO2eq/kg or l of food ITEM'] &&
+      parseFloat(
+        item['Carbon Footprint kg CO2eq/kg or l of food ITEM'].replace(',', '.')
+      ),
+    typology: item['Food commodity TYPOLOGY'],
+  }));
+
+  const index = Fuse.createIndex(['item'], parsedDb);
+  const fuse = new Fuse(parsedDb, {}, index);
+
   const productScores = req.body.products.map((p) => {
+    const results = fuse.search(p.label, { limit: 1 });
+
     return {
       quantity: p.quantity,
       label: p.label,
-      score: Math.random(),
+      score: results.length > 0 ? results[0].item.footprint : null,
     };
   });
 
@@ -21,8 +48,11 @@ handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
     data: {
       products: productScores,
       score:
-        productScores.reduce((acc, p) => acc + p.quantity * p.score, 0) /
-        productScores.reduce((acc, p) => acc + p.quantity, 0),
+        productScores.reduce(
+          (acc, p) => (p.score ? acc + p.quantity * p.score : acc),
+          0
+        ) /
+        productScores.reduce((acc, p) => (p.score ? acc + p.quantity : acc), 0),
     },
   });
 });
