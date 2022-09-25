f4d5525163cdb3a6f2885b15e3151a8c95694b07
Felix Wotschofsky <hello@felisk.io>
2022-09-25 02:03:36 +0200

    Format code ✨

--- pages/all.tsx
+++ pages/all.tsx
@@ -6,10 +6,10 @@ export default function Page({ receipts }: any) {
     return <div>
         {receipts.map(i => <a style={{ padding: "1rem" }} href={`/receipts/${i._id}`}><MiniReceipt color="red" /></a>)}
     </div>
+  );
 }
 export const getServerSideProps = async () => {
+  const receipts = await apiClient().getAllReceipts();
 
-    const receipts = await apiClient().getAllReceipts()
-
-    return { props: { receipts } }
-}
+  return { props: { receipts } };
+};
