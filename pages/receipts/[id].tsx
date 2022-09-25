449fe09f8f6ed092202b062a6b15d3d28fe02ee3
Felix Wotschofsky <hello@felisk.io>
2022-09-25 12:54:29 +0200

    Convert receipt detail page SSR to direct query 🚚

--- pages/receipts/[id].tsx
+++ pages/receipts/[id].tsx
@@ -1,19 +1,16 @@
 import Head from 'next/head';
-import { FaInstagram, FaTwitter, FaWhatsapp } from 'react-icons/fa';
-import Button from '../../components/Button';
+import { FaTwitter, FaWhatsapp } from 'react-icons/fa';
 
 import style from '../../components/iconButton.module.css';
-import apiClient from '../../services/apiClient';
+import receiptController from '../../controllers/receipt.controller';
+import initMongoose from '../../utils/initMongoose';
 
 export default function Page({ receipt }: any) {
+  const text = encodeURIComponent('moin master die alwkejr');
+  const url = encodeURIComponent('https://ceipt.app/receipts/' + receipt._id);
 
-  const text = encodeURIComponent('moin master die alwkejr')
-
-  const url = encodeURIComponent('https://ceipt.app/receipts/' + receipt._id)
-
-  const twitterShareLink = `https://twitter.com/intent/tweet?text=${text}&url=${url}`
-
-  const whatsappShareLink = `whatsapp://send?text=${text}`
+  const twitterShareLink = `https://twitter.com/intent/tweet?text=${text}&url=${url}`;
+  const whatsappShareLink = `whatsapp://send?text=${text}`;
 
   return (
     <>
@@ -35,19 +32,22 @@ export default function Page({ receipt }: any) {
           display: 'flex',
           alignItems: 'center',
           flexDirection: 'column',
-          width: "20rem"
+          width: '20rem',
         }}
       >
-        <img src={`/api/v1/assets/receipts/${receipt._id}`} width="100%"  style={{paddingBottom: "0.6rem"}}/>
+        <img
+          src={`/api/v1/assets/receipts/${receipt._id}`}
+          width="100%"
+          style={{ paddingBottom: '0.6rem' }}
+        />
 
         <a
           href={twitterShareLink}
           target="_blank"
           rel="noreferrer"
-          style={{width: "100%"}}
+          style={{ width: '100%' }}
         >
           <FaTwitter className={style.iconButton} />
-          
         </a>
 
         {/* <FaInstagram className={style.iconButton} /> */}
@@ -57,7 +57,7 @@ export default function Page({ receipt }: any) {
           data-action="share/whatsapp/share"
           target="_blank"
           rel="noreferrer"
-          style={{width: "100%"}}
+          style={{ width: '100%' }}
         >
           <FaWhatsapp className={style.iconButton} />
         </a>
@@ -69,7 +69,8 @@ export default function Page({ receipt }: any) {
 export const getServerSideProps = async ({ query }: any) => {
   const { id } = query;
 
-  const receipt = await apiClient().getReceiptById(id);
+  await initMongoose();
+  const receipt = await receiptController.getById(id);
 
   return {
     props: {
