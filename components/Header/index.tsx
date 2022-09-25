62714093e260bf2ccb6cf76a536cf2b5fa20933d
Felix Wotschofsky <hello@felisk.io>
2022-09-25 05:10:24 +0200

    Replace a tags with Next.js Links 🔗

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import style from './header.module.css';
 
 export default function Header() {
@@ -6,16 +8,16 @@ export default function Header() {
       <img src="/logo.png" className={style.logo} />
 
       <nav className={style.nav}>
-        <a href="#" className={style.headerLink}>
+        <Link href="#" className={style.headerLink}>
           Imprint
-        </a>
+        </Link>
 
-        <a href="/all" className={style.headerLink}>
-            All Receipts
-        </a>
-        <a href="/" className={style.headerLink}>
-            Upload Receipt
-        </a>
+        <Link href="/all" className={style.headerLink}>
+          All Receipts
+        </Link>
+        <Link href="/" className={style.headerLink}>
+          Upload Receipt
+        </Link>
       </nav>
     </header>
   );
