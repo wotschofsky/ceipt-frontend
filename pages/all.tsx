47f085aa456d4801ba543e3f17911dd19a59b59c
Felix Wotschofsky <hello@felisk.io>
2022-09-25 03:49:06 +0200

    fix

--- pages/all.tsx
+++ pages/all.tsx
@@ -1,10 +1,14 @@
-import MiniReceipt from "../components/MiniReceipt"
-import apiClient from "../services/apiClient"
+import MiniReceipt from '../components/MiniReceipt';
+import apiClient from '../services/apiClient';
 
 export default function Page({ receipts }: any) {
-
-    return <div>
-        {receipts.map(i => <a style={{ padding: "1rem" }} href={`/receipts/${i._id}`}><MiniReceipt color="red" /></a>)}
+  return (
+    <div>
+      {receipts.map((i) => (
+        <a style={{ padding: '1rem' }} href={`/receipts/${i._id}`}>
+          <MiniReceipt color="red" />
+        </a>
+      ))}
     </div>
   );
 }
